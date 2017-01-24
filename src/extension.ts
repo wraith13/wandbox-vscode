e6949ba344384c2236dc377023200b6ecc9ff67e
道化師 <wraith@trickpalace.net>
2017-01-24 10:22:56 +0900

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -6,83 +6,128 @@ import * as request from 'request';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext)
+{
 
     var outputChannel :vscode.OutputChannel;
+    var makeSureOutputChannel = () =>
+    {
+        if (!outputChannel)
+        {
+            outputChannel = vscode.window.createOutputChannel('Wandbox');
+        }
+        else
+        {
+            outputChannel.appendLine('');
+        }
+        return outputChannel;
+    }
+    var bowWow = () =>
+    {
+        outputChannel.show();
+        outputChannel.appendLine('Bow-wow! ' + new Date().toString());
+    }
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "wandbox-vscode" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-    context.subscriptions.push(disposable);
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand('extension.getWandboxList', () => {
-            if (!outputChannel) {
-                outputChannel = vscode.window.createOutputChannel('Wandbox');
-            }
-            outputChannel.show();
-            outputChannel.appendLine('');
-            outputChannel.appendLine('Bow-wow! ' + new Date().toString());
-
-            outputChannel.appendLine('HTTP GET http://melpon.org/wandbox/api/list.json');
-            request.get('http://melpon.org/wandbox/api/list.json', function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var provider = vscode.workspace.registerTextDocumentContentProvider
+    context.subscriptions.push
+    (
+        vscode.commands.registerCommand
+        (
+            'extension.openWandboxWeb',
+            () =>
+            {
+                vscode.commands.executeCommand
                 (
-                    'wandbox-list-json',
-                    new class implements vscode.TextDocumentContentProvider {
-                        onDidChange?: vscode.Event<vscode.Uri>;
-                        provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): string | Thenable<string>
+                    'vscode.open',
+                    vscode.Uri.parse('http://melpon.org/wandbox/?from=wandbox-vscode')
+                );
+            }
+        )
+    );
+    context.subscriptions.push
+    (
+        vscode.commands.registerCommand
+        (
+            'extension.getWandboxList',
+            () =>
+            {
+                makeSureOutputChannel();
+                bowWow();
+
+                outputChannel.appendLine('HTTP GET http://melpon.org/wandbox/api/list.json?from=wandbox-vscode');
+                request.get
+                (
+                    'http://melpon.org/wandbox/api/list.json',
+                    function (error, response, body)
+                    {
+                        if (!error && response.statusCode == 200)
                         {
-                            return JSON.stringify(JSON.parse(body), null, 4);
+                            var provider = vscode.workspace.registerTextDocumentContentProvider
+                            (
+                                'wandbox-list-json',
+                                new class implements vscode.TextDocumentContentProvider
+                                {
+                                    provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken)
+                                        : string | Thenable<string>
+                                    {
+                                        return JSON.stringify(JSON.parse(body), null, 4);
+                                    }
+                                }
+                            );
+                            vscode.workspace.openTextDocument
+                            (
+                                vscode.Uri.parse('wandbox-list-json://melpon.org/wandbox-api-list.json')
+                            )
+                            .then
+                            (
+                                (value: vscode.TextDocument) =>
+                                {
+                                    vscode.window.showTextDocument(value);
+                                    provider.dispose();
+                                }
+                            );
+                        }
+                        else
+                        if (response.statusCode)
+                        {
+                            outputChannel.appendLine('statusCode: ' +response.statusCode);
+                        }
+                        else
+                        {
+                            outputChannel.appendLine('error: ' +error);
                         }
                     }
                 );
-                vscode.workspace.openTextDocument(vscode.Uri.parse('wandbox-list-json://melpon.org/wandbox-api-list.json')).then(
-                    (value: vscode.TextDocument)=>{
-                        vscode.window.showTextDocument(value);
-                        provider.dispose();
-                    }
-                )
-            } else if (response.statusCode) {
-                outputChannel.appendLine('statusCode: ' +response.statusCode);
-            } else {
-                outputChannel.appendLine('error: ' +error);
             }
-            });
-        })
+        )
     );
-    context.subscriptions.push(
-        vscode.commands.registerCommand('extension.invokeWandbox', (args :any[]) => {
-            if (!outputChannel) {
-                outputChannel = vscode.window.createOutputChannel('Wandbox');
-            }
-            outputChannel.show();
-            outputChannel.appendLine('');
-            outputChannel.appendLine('Bow-wow! ' + new Date().toString());
-
-            args && args.forEach(arg => {
-                outputChannel.appendLine('arg: ' +arg);
-            });
-
-            var activeTextEditor = vscode.window.activeTextEditor;
-            if (null !== activeTextEditor)
+    context.subscriptions.push
+    (
+        vscode.commands.registerCommand
+        (
+            'extension.invokeWandbox',
+            (args :any[]) =>
             {
-                outputChannel.appendLine('fileName: ' +activeTextEditor.document.fileName);
-                outputChannel.appendLine('text: ' +activeTextEditor.document.getText());
-                outputChannel.appendLine('languageId: ' +activeTextEditor.document.languageId);
+                makeSureOutputChannel();
+                bowWow();
+
+                args && args.forEach
+                (
+                    arg => outputChannel.appendLine('arg: ' +arg)
+                );
+
+                var activeTextEditor = vscode.window.activeTextEditor;
+                if (null !== activeTextEditor)
+                {
+                    outputChannel.appendLine('fileName: ' +activeTextEditor.document.fileName);
+                    outputChannel.appendLine('text: ' +activeTextEditor.document.getText());
+                    outputChannel.appendLine('languageId: ' +activeTextEditor.document.languageId);
+                }
             }
-        })
+        )
     );
 }
 
