acef44c68dfe8dc9cb12d67bbe0d58b7e6738878
道化師 <wraith@trickpalace.net>
2017-02-03 19:15:17 +0900

    不要なコードの除去

--- src/extension.ts
+++ src/extension.ts
@@ -160,10 +160,6 @@ export function activate(context: vscode.ExtensionContext)
         }
     };
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log(`Congratulations, your extension "${extentionName}" is now active!`);
-
     context.subscriptions.push
     (
         vscode.commands.registerCommand
