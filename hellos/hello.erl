6ea035047a483a7c4ee390c88bc3d131fa8146f9
道化師 <wraith@trickpalace.net>
2017-02-12 04:13:40 +0900

    Hello, world! ファイル ( erl )

---
+++ hellos/hello.erl
@@ -0,0 +1,7 @@
+% This file is a "Hello, world!" in CoffeeScript for wandbox-vscode.
+
+main(_) ->
+    io:format("Hello, Wandbox!~n").
+
+% Erlang Reference
+% http://erlang.org/doc/reference_manual/users_guide.html
