fcbe87f97db28248768754f0ab37123b892d78d1
道化師 <wraith@trickpalace.net>
2017-02-12 02:01:01 +0900

    Hello, world! ファイル ( c, cpp, cxx, sh )

---
+++ hellos/hello.cpp
@@ -0,0 +1,18 @@
+//  This file is a "Hello, world!" in C++ language for wandbox-vscode.
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    std::cout << "Hello, Wandbox!" << std::endl;
+    return EXIT_SUCCESS;
+}
+
+//  C++ language references:
+//  https://msdn.microsoft.com/library/3bstk3k5.aspx
+//  http://www.cplusplus.com/
+//  https://isocpp.org/
+//  http://www.open-std.org/jtc1/sc22/wg21/
+
+//  Boost libraries references:
+//  http://www.boost.org/doc/
