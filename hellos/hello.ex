f5d0a267b3577da3b97afd8e0ce7e1b9a65b22b4
道化師 <wraith@trickpalace.net>
2017-02-12 20:13:32 +0900

    Hello, world! ファイル ( ex, exs, groovy, gvy )

---
+++ hellos/hello.ex
@@ -0,0 +1,13 @@
+#!/usr/bin/env elixir
+# This file is a "Hello, world!" in Elixir language for wandbox-vscode.
+defmodule Wandbox do
+
+    def vscode do
+        IO.puts "Hello, Wandbox!"
+    end
+end
+
+Wandbox.vscode
+
+# Elixir language references:
+# http://elixir-lang.org
