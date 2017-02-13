14968ee0ca7f21c8d98a447a3317839ba0daa748
道化師 <wraith@trickpalace.net>
2017-02-13 13:40:05 +0900

    Hello, world! ファイル ( rb, rs, scala )

---
+++ hellos/hello.rb
@@ -0,0 +1,6 @@
+# This file is a "Hello, world!" in Ruby language for wandbox-vscode.
+
+puts "Hello, Wandbox!"
+
+# Ruby language references:
+# https://docs.ruby-lang.org/
---
+++ hellos/hello.scala
@@ -0,0 +1,12 @@
+//  This file is a "Hello, world!" in Scala language for wandbox-vscode.
+
+object WandboxVSCode
+{
+    def main(args: Array[String])
+    {
+        println("Hello, Wandbox!")
+    }
+}
+
+//  Scala language references:
+//  http://www.scala-lang.org
