e16eb784be9e426b3c7c0c42bdaf193ef10765da
道化師 <wraith@trickpalace.net>
2017-02-13 21:34:27 +0900

    Hello, world! ファイル ( sql, swift, vim )

---
+++ hellos/hello.sql
@@ -0,0 +1,7 @@
+-- This file is a "Hello, world!" in SQL for wandbox-vscode.
+
+SELECT 'Hello, Wandbox!';
+
+-- SQL references:
+-- https://www.mysql.com
+-- https://msdn.microsoft.com/library/dn198336.aspx
---
+++ hellos/hello.swift
@@ -0,0 +1,6 @@
+//  This file is a "Hello, world!" in Apple Swift language for wandbox-vscode.
+
+print("Hello, Wandbox!");
+
+//  Apple Swift language references:
+//  http://www.apple.com/swift/
