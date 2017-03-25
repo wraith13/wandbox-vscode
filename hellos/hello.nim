635180bd13daedef24f27de34dfce6986ceca328
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-25 22:37:57 +0900

    add Nim

---
+++ hellos/hello.nim
@@ -0,0 +1,7 @@
+# This file is a "Hello, world!" in Nim language for wandbox-vscode.
+
+echo "Hello, Wandbox!"
+
+# Nim language references:
+# https://nim-lang.org/
+# https://nim-lang.org/docs/manual.html
